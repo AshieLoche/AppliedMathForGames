8f425b4b322aa9fdec4ee8e56f37028a8dd3198e
CIIT601PC01\Student <it@ciit.edu.ph>
2024-05-20 10:45:18 +0800

    week5 initial

---
+++ Assets/Scripts/Week5/Enemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    Vector3 lastSeen;
+    Vector3 target;
+
+    public GameObject player;
+    public float ViewDistance;
+    public float ViewAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastSeen = transform.position;
+        target = player.transform.position;
+        // When we reach our target
+        if (Vector3.Distance(lastSeen, target) < 0.5f)
+        {
+            // At target, pick up new spot to go to
+            Quaternion rotation = Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+            target = lastSeen + (rotation * transform.up * 5.0f);
+        }
+        else
+        {
+            Vector3 dir = target - transform.position;
+            float angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+            Quaternion p = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Lerp(transform.rotation, p, Time.deltaTime * 10.0f);
+
+            transform.position += 5.0f * Time.deltaTime * transform.right;
+        }
+
+        Debug.DrawRay(transform.position, transform.up * 5.0f, Color.yellow);
+
+    }
+}
---
+++ Assets/Scripts/Week5/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += 100.0f * Time.deltaTime * transform.up;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += 100.0f * Time.deltaTime * -transform.up;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(transform.forward, Time.deltaTime * 200.0f);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(transform.forward, Time.deltaTime * -200.0f);
+        }
+
+        Debug.DrawRay(transform.position, transform.up * 5.0f, Color.blue);
+        
+    }
+}
