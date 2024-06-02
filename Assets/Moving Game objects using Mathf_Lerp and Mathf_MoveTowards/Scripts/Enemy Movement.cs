03036a108074ccfa32b87c908973ea1bfa5af15a
Ashie Loche <134357122+AshieLoche@users.noreply.github.com>
2024-06-02 18:40:46 +0800

    Assignment Moving Game objects using Mathf.Lerp and Mathf.MoveTowards - Update

--- Assets/Moving Game objects using Mathf_Lerp and Mathf_MoveTowards/Scripts/Enemy Movement.cs
+++ Assets/Moving Game objects using Mathf_Lerp and Mathf_MoveTowards/Scripts/Enemy Movement.cs
@@ -8,25 +8,30 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Transform player;
+    [SerializeField] private Animator animator;
     [SerializeField] private float speed;
-    private float distance;
-    [SerializeField] private float distanceThreshhold;
+    [SerializeField] private float catchArea;
     [SerializeField] private float rotationAngle;
+    private float distance;
+    private bool chase = true;
+    private bool catched = false;
+    public bool Catched { get { return catched; } }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            spriteRenderer.flipX = !spriteRenderer.flipX;
-        }
 
-        if (player)
+        if (player && chase)
         {
+            animator.SetTrigger("Chase");
+
             distance = Mathf.Sqrt(Mathf.Pow(player.position.x - transform.position.x, 2) + Mathf.Pow(player.position.y - transform.position.y, 2));
 
-            if (distance > distanceThreshhold)
+            catched = distance < catchArea;
+
+            if (!catched)
             {
+                animator.SetBool("Attack", false);
                 Vector3 direction = player.position - transform.position;
                 rotationAngle = Mathf.Atan2(direction.y, direction.x) * 180 / Mathf.PI;
 
@@ -42,10 +47,23 @@ public class EnemyMovement : MonoBehaviour
             }
             else
             {
-                Destroy(player.gameObject);
+                StartCoroutine(Attack(0.6f));
             }
         }
 
+    }
+
+    private IEnumerator Attack(float waitTime)
+    {
+        chase = false;
+        animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(waitTime);
+        animator.SetTrigger("Kill");
+        Destroy(player.gameObject);
+        yield return new WaitForSeconds(waitTime/6.0f);
+        animator.ResetTrigger("Chase");
+        animator.ResetTrigger("Attack");
+        animator.ResetTrigger("Kill");
     }
 
 }
--- Assets/Moving Game objects using Mathf_Lerp and Mathf_MoveTowards/Scripts/Player Movement.cs
+++ Assets/Moving Game objects using Mathf_Lerp and Mathf_MoveTowards/Scripts/Player Movement.cs
@@ -1,51 +1,111 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    [SerializeField] private Transform up;
-    [SerializeField] private Transform down;
-    [SerializeField] private Transform left;
-    [SerializeField] private Transform right;
-    [SerializeField] private Transform sprite;
+    //[SerializeField] private Transform up;
+    //[SerializeField] private Transform down;
+    //[SerializeField] private Transform left;
+    //[SerializeField] private Transform right;
+    //[SerializeField] private Transform dash;
+    //[SerializeField] private float speed;
+    //[SerializeField] private float dashSpeed;
+
+    [SerializeField] private EnemyMovement enemyMovement;
+    [SerializeField] private GameObject sprite;
+    [SerializeField] private Transform spriteTransform;
     [SerializeField] private SpriteRenderer spriteRenderer;
-    [SerializeField] private float speed;
     [SerializeField] private Animator animator;
+    [SerializeField] private Transform walkWaypoint;
+    [SerializeField] private Transform dashWayPoint;
+    [SerializeField] private Vector3 dashPosition;
+    [SerializeField] private float speed;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private bool dashing = false;
+    private Vector3 movement;
+    private int axis;
+
+    private void Start()
+    {
+        spriteTransform = sprite.transform;
+        spriteRenderer = sprite.GetComponent<SpriteRenderer>();
+        animator = sprite.GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 movement = new Vector3(transform.position.x, Mathf.MoveTowards(transform.position.y, up.position.y, speed * Time.deltaTime), 0.0f);
-            sprite.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
-            spriteRenderer.flipX = false;
-            transform.position = movement;
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            Vector3 movement = new Vector3(transform.position.x, Mathf.MoveTowards(transform.position.y, down.position.y, speed * Time.deltaTime), 0.0f);
-            sprite.rotation = Quaternion.Euler(0.0f, 0.0f, -90.0f);
-            spriteRenderer.flipX = false;
-            transform.position = movement;
-        }
-        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            Vector3 movement = new Vector3(Mathf.MoveTowards(transform.position.x, left.position.x, speed * Time.deltaTime), transform.position.y, 0.0f);
-            sprite.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
-            spriteRenderer.flipX = true;
-            transform.position = movement;
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            Vector3 movement = new Vector3(Mathf.MoveTowards(transform.position.x, right.position.x, speed * Time.deltaTime), transform.position.y, 0.0f);
-            sprite.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
-            spriteRenderer.flipX = false;
-            transform.position = movement;
+            dashing = true;
+            dashPosition = dashWayPoint.position;
         }
 
+        if (dashing && !enemyMovement.Catched)
+        {
+            animator.SetBool("Run", true);
+            animator.speed = 2.5f;
+            movement = (axis == 0) ? new Vector3(Mathf.Lerp(transform.position.x, dashPosition.x, dashSpeed * Time.deltaTime), transform.position.y, 0.0f) : new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, dashPosition.y, dashSpeed * Time.deltaTime), 0.0f);
+            transform.position = movement;
+            StartCoroutine(Dash());
+        }
+        else
+        {
+            if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && !enemyMovement.Catched)
+            {
+                animator.SetBool("Run", true);
+
+                if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+                {
+                    axis = 1;
+                    transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
+                    spriteRenderer.flipY = false;
+                }
+                else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+                {
+                    axis = 1;
+                    transform.rotation = Quaternion.Euler(0.0f, 0.0f, -90.0f);
+                    spriteRenderer.flipY = false;
+                }
+                else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                {
+                    axis = 0;
+                    transform.rotation = Quaternion.Euler(0.0f, 0.0f, 180.0f);
+                    spriteRenderer.flipY = true;
+                }
+                else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                {
+                    axis = 0;
+                    transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+                    spriteRenderer.flipY = false;
+                }
+
+                Move();
+            }
+            else
+            {
+                animator.SetBool("Run", false);
+            }
+        }
+
+    }
+
+    private void Move()
+    {
+        movement = (axis == 0) ? new Vector3(Mathf.MoveTowards(transform.position.x, walkWaypoint.position.x, speed * Time.deltaTime), transform.position.y, 0.0f) : new Vector3(transform.position.x, Mathf.MoveTowards(transform.position.y, walkWaypoint.position.y, speed * Time.deltaTime), 0.0f);
+        transform.position = movement;
+    }
+
+    IEnumerator Dash()
+    {
+        yield return new WaitUntil(() => enemyMovement.Catched || Mathf.Abs(transform.position.x - dashPosition.x) <= 1f || Mathf.Abs(transform.position.y - dashPosition.y) <= 1f);
+        animator.SetBool("Run", false);
+        yield return new WaitUntil(() => enemyMovement.Catched || transform.position == dashPosition);
+        dashing = false;
+        animator.speed = 1;
     }
 }
